1817f8b932631872390fb941cd1b8db45b8a6af0
Alexandre Gomes Goncalves <alexandreggoncalves@hotmail.com>
2024-10-22 10:46:50 -0400

    update 22 10 2024

--- meuestoquefacil/settings.py
+++ meuestoquefacil/settings.py
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authuser',
+    'usuario',
     'sistema',
     'area',
     'perfil',
 ]
+
+AUTH_USER_MODEL = 'authuser.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
