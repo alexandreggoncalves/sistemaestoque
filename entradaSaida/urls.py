e763a75a91914fbc72b9a9aed5e7ad9f1dd42091
Alexandre Gomes Goncalves <alexandreggoncalves@hotmail.com>
2024-10-29 10:59:55 -0400

    entrada e saída insert do ajax de busca

--- entradaSaida/urls.py
+++ entradaSaida/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registrarEntradaSaida', views.registrarEntradaSaida, name='registrarEntradaSaida'),
+    path('registrarEntradaSaida/', views.registrarEntradaSaida, name='registrarEntradaSaida'),
+    path('buscarProdutos/', views.buscarProdutos, name='buscarProdutos'),
 ]
--- entradaSaida/views.py
+++ entradaSaida/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
 from django.core.paginator import Paginator
 from .forms import EntradaSaidaForm
 from .models import EntradaSaida
+from produto.models import Produto
 
 # INSERIR ENTRADA/SAIDA
 @login_required(login_url='/sistema/login/') 
@@ -10,3 +12,18 @@ def registrarEntradaSaida(request):
     
     form = EntradaSaidaForm()
     return render(request, 'registrarEntradaSaida.html', { 'form': form})
+
+# BUSCA DE PRODUTOS
+@login_required(login_url='/sistema/login/') 
+def buscarProdutos(request):
+
+    # realiza a busca na tabela de produtos
+    produtos = Produto.objects.filter(nomeProduto__icontains=request.GET.get('txtBusca')).order_by('nomeProduto')
+    
+    # cria um dicionáiro para o retorno da busca e aciona o resultado.
+    dataReturn = {}
+    for produtos in produtos: # adicona os resultados no dicionario
+        dataReturn[produtos.idProduto] = produtos.nomeProduto
+
+    # retorna os dados via Json
+    return JsonResponse(dataReturn)
